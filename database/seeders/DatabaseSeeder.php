fe9079a6ceeb1176ce045342de96b6efe5dd493e
Davith <davith@MacBook-Air-de-Coline.local>
2021-11-15 10:07:09 +0100

    edit readme

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
     {
 
         $this->call([
-            CourseSeeder::class,
             UserSeeder::class,
+            CourseSeeder::class,
             ChapterSeeder::class,
             CategorySeeder::class,
         ]);
